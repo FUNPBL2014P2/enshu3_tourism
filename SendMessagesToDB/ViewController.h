6082af5de59ebad34685737ed47b264196a2f3f1
Takuma Sasaki <b1012216@fun.ac.jp>
2014-06-02 20:25:53 +0900

    nameTextfieldを追加
    nameTexfieldを.hファイルに繋いだ

--- SendMessagesToDB/ViewController.h
+++ SendMessagesToDB/ViewController.h
@@ -9,5 +9,7 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+- (IBAction)nameTextfield:(id)sender;
+@property (weak, nonatomic) IBOutlet UITextField *nameTextfield;
 
 @end
